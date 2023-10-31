6f3d0c3c3ccbfd3a9855776cd12ffbd8f1aceafa
Pankhuri Singh <119196051+Pankhuri0824@users.noreply.github.com>
2023-11-01 00:07:22 +0530

    Create Q5.py

---
+++ Assignment 3/Q5/Q5.py
@@ -0,0 +1,139 @@
+course="IP"
+credits=4
+assesment=[("midsem",40),("endsem",60)]
+policy = [80,65,50,40,30]
+
+f=open("./Q4.txt","r")
+# midsem-40 , endsem-60
+d={}
+midsem={}
+endsem={}
+marks=[]
+for lines in f:
+    a=lines.split()
+    midsem[int(a[0])]=int(a[1])
+    endsem[int(a[0])]=int(a[2])
+    total=int(a[1])+(int(a[2]))
+    d[int(a[0])]=total
+    marks.append(total)
+def grading1(l):
+    # A>80....80>B>65....65>C>50....50>D>40...40>F
+    A=[]
+    B=[]
+    C=[]
+    D=[]
+    for i in l:
+        if 82>i>78:
+            A.append(i)
+        elif 67>i>63:
+            B.append(i)
+        elif 52>i>48:
+            C.append(i)
+        elif 42>i>38:
+            D.append(i)
+    A.sort()
+    B.sort()
+    C.sort()
+    D.sort()
+    grade=[A,B,C,D]
+    cutoff={}
+    gr={0:"A",1:"B",2:"C",3:"D"}
+    a=0
+    for k in grade:
+        a1=gr[a]
+        g={}
+        if k==[]:
+            cutoff[a1]=policy[a]
+        else:
+            if len(k)>1:
+                for i in range (0,len(k)-1):
+                        g[k[i],k[i+1]]=abs(k[i]-k[i+1])
+                l=list(g.values())
+                m=max(l)
+                val=[i for i in g.keys() if g[i]==m]
+                for i in val:
+                    s=0
+                    for y in range (len(i)):
+                        s+=i[y]
+                cuto=s/2
+                cutoff[a1]=cuto
+            else:
+                for i in k:
+                    cutoff[a1]=i
+        a+=1
+    return cutoff
+cutoff=grading1(marks)
+# policy = [80,65,50,40,30]
+keys=list(cutoff.keys())
+keys.append("F")
+values=list(cutoff.values())
+values.append(0)
+def grading(n):
+    grdes=["A","B","C","D","F"]
+    if n>=values[0]:
+        return grdes[0]
+    elif values[0]>n>=values[1]:
+        return grdes[1]
+    elif values[1]>n>=values[2]:
+        return grdes[2]
+    elif values[2]>n>=values[3]:
+        return grdes[3]
+    elif values[3]>n>=values[4]:
+        return grdes[4]
+    
+t=True
+while t:
+    n=(input("Enter command:"))
+    if n=="":
+        t=False
+        print("Program Terminated")
+        break
+    n=int(n)
+    if n==1:
+        # course name, credits, assesment and weight, grade cutoffs, grading summary of students( 3 As, 4 Bs, etc)
+        print("Course name:",course)
+        print("Credits:",credits)
+        print("Assesment:",assesment)
+        print("Grade cutoffs for the course are as follows:")
+        for i in range (len(cutoff)+1):
+            if i==0:
+                print(f'{keys[i]} : {values[i]}-100')
+            else:
+                print(f'{keys[i]} : {values[i]}-{values[i-1]}')
+        key=list(d.keys())
+        value=list(d.values())
+        ga,gb,gc,gd,gf=0,0,0,0,0
+        for i in range (len(key)):
+            x=grading(value[i])
+            if x=="A":
+                ga+=1
+            elif x=="B":
+                gb+=1
+            elif x=="C":
+                gc+=1
+            elif x=="D":
+                gd+=1
+            elif x=="F":
+                gf+=1
+        lis=[ga,gb,gc,gd,gf]
+        grdes=["A","B","C","D","F"]
+        print("Grading summary is as follows:")
+        for i in range (5):
+            print(f"Number of {grdes[i]}'s : {lis[i]}")
+
+    elif n==2:
+        # Print grades in a file by: Roll No.,Total marks, grade
+        key=list(d.keys())
+        value=list(d.values())
+        f1=open("./Q5Output.txt","w")
+        for i in range (len(key)):
+            f1.write(f'Roll Number :{key[i]}, Total marks:{value[i]},Grade: {grading(value[i])}'+"\n")
+
+    elif n==3:
+        #Search student record : Input- Roll number
+        # Print- Marks in diff assesemnts, Total marks, Final grade
+        a=int(input("Enter Roll Number:"))
+        print(f'Marks by assesments : 1)Midsem:{midsem[a]}, 2)Endsem:{endsem[a]}')
+        print("Total Marks:",d[a])
+        print("Final grade",grading(d[a]))
+
